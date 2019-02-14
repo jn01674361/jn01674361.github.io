40570262e6d15e0fe96d00b30f8fddc4a9824d29
jn01674361 <joar.nykvist@gmail.com>
2019-02-14 13:38:11 +0100

    Fix spanning background pic

---
+++ myproject/src/components/Agent0.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Agent0">
+    <iframe
+      allow="microphone;"
+      width="700"
+      height="500"
+      text-align="center"
+      src="https://console.dialogflow.com/api-client/demo/embedded/c480f8ac-06eb-4aeb-9794-be0219f64243">
+    </iframe>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Agent0',
+  data () {
+    return {
+      msg: 'Agent0 is upp and running'
+    }
+  }
+}
+</script>
+<style>
+  .center {
+  text-align: center;
+  border: 3px solid green;
+  }
+</style>
---
+++ myproject/src/components/bg.vue
@@ -0,0 +1,16 @@
+<template>
+    <div background="one_red_chili.jpg">
+        <div id="Agent0">here should be an agent</div>
+        <div id="Rating">here should be ratings</div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'bg',
+  data () {
+    return {
+      msg: 'bg is a chili'
+    }
+  }
+}
+</script>
